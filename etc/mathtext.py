ba604d3cacc436ba92399dfc0aa5769732380366
Alexandre Bouquin <abouquin@fis.ucm.es>
2016-11-22 22:36:26 +0100

    v0.7.0 minor update. commented out mathtext.py because of tabulate module.

--- etc/mathtext.py
+++ etc/mathtext.py
@@ -1,10 +1,10 @@
 ### Use HTML and MathJax syntax
-from tabulate import tabulate
-
-def textcalcout(sourcet, inputcontt):
-    string = "The source is %s <BR />" % (sourcet)
-    string += "The input continuum is %s <BR />" % (inputcontt)
-    string += "$$ \omega$$ \n"
-    string += tabulate(locals(), headers="keys")
-    print string
-    return string
+# from tabulate import tabulate
+#
+# def textcalcout(sourcet, inputcontt):
+#     string = "The source is %s <BR />" % (sourcet)
+#     string += "The input continuum is %s <BR />" % (inputcontt)
+#     string += "$$ \omega$$ \n"
+#     string += tabulate(locals(), headers="keys")
+#     print string
+#     return string
